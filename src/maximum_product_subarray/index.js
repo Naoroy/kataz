5ca232d823422ae94b63840b16a76c73e654f059
Fégan Pierre <fegan.pierre@gmail.com>
2022-06-13 11:12:00 +0200

    attempt

--- src/maximum_product_subarray/index.js
+++ src/maximum_product_subarray/index.js
@@ -1,5 +1,37 @@
-function func() {
+function brute(nums) {
+  let max
+  let ln = nums.length
 
+  for (let i = 0; i < ln; i++) {
+    let prod = 1
+    for (let j = i; j < ln; j++) {
+      prod *= nums[j]
+      max = prod < max ? max : prod
+    }
+  }
+
+  return max == 0 ? 0 : max
 }
 
-module.exports = func
+function maximumProductSubarray(nums) {
+  let product
+  let tmpProduct = 1
+  let i = 0
+
+  while (i < nums.length) {
+    if (nums[i] > nums[i] * tmpProduct) {
+      tmpProduct = nums[i]
+    } else {
+      tmpProduct = nums[i] * tmpProduct
+    }
+
+    if (!product || product <= tmpProduct) {
+      product = tmpProduct
+    }
+    i++
+  }
+
+  return product
+}
+
+module.exports = { brute, maximumProductSubarray }
