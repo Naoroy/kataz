e525c8a3b4abaf175b08985bb8720ec605ed03b9
Fégan Pierre <fegan.pierre@gmail.com>
2022-06-27 11:22:26 +0200

    solve problem

--- src/704.Binary_search/index.js
+++ src/704.Binary_search/index.js
@@ -1,26 +1,32 @@
 function func(nums, target) {
-  const value = -1
-  const map = buildMap(nums)
-
-  const half = Math.floor(nums.length / 2)
-
-  if (nums[half] == target) {
-
-  }
-  const left = nums.slice(0, half)
-  const right = nums.slice(half)
-
-  return value
-}
-
-function buildMap(n) {
   const map = {}
 
-  n.forEach((x, i) => {
-    map[x] = i
-  })
+  nums.forEach((x, i) => map[x] = i)
 
-  return map
+  if (map[target] == undefined) return -1
+
+  return map[target]
 }
 
-module.exports = { func }
+function divideAndConquer(nums, target) {
+  const half = Math.floor(nums.length / 2)
+  let cut
+  let map = {}
+
+  if (nums.length <= 0) return -1
+  if (nums[half] == target) return half
+
+  cut = nums[half] > target
+    ? nums.slice(0, half)
+    : nums.slice(half)
+
+  cut.forEach((x, i) => map[x] = i)
+
+  if (map[target] == undefined) return -1
+
+  return nums[half] > target
+    ? map[target]
+    : map[target] + half
+}
+
+module.exports = { func, divideAndConquer }
--- src/704.Binary_search/index.test.js
+++ src/704.Binary_search/index.test.js
@@ -1,4 +1,4 @@
-const { func } = require('./index')
+const { func, divideAndConquer } = require('./index')
 
 describe('Function', function() {
   test('Function exists', function() {
@@ -22,5 +22,56 @@ describe('Function', function() {
 
     expect(result).toEqual(expectetResult)
   })
-  
+  test('Other examples', function() {
+    const testCase = [5]
+    const testTarget = 5
+    const expectetResult = 0
+
+    const result = func(testCase, testTarget)
+
+    expect(result).toEqual(expectetResult)
+  })
+})
+
+describe.only('Divide and conquer', function() {
+  test('Function exists', function() {
+    expect(divideAndConquer).toBeTruthy()
+  })
+  test('Example 1', function() {
+    const testCase = [-1,0,3,5,9,12]
+    const testTarget = 9
+    const expectetResult = 4
+
+    const result = divideAndConquer(testCase, testTarget)
+
+    expect(result).toEqual(expectetResult)
+  })
+  test('Example 2', function() {
+    const testCase = [-1,0,3,5,9,12]
+    const testTarget = 2
+    const expectetResult = -1
+
+    const result = divideAndConquer(testCase, testTarget)
+
+    expect(result).toEqual(expectetResult)
+  })
+  test('Other examples', function() {
+    const testCase = [5]
+    const testTarget = 5
+    const expectetResult = 0
+
+    const result = divideAndConquer(testCase, testTarget)
+
+    expect(result).toEqual(expectetResult)
+  })
+  test('Other examples', function() {
+    const testCase = [2,5]
+
+    const testTarget = 2
+    const expectetResult = 0
+
+    const result = divideAndConquer(testCase, testTarget)
+
+    expect(result).toEqual(expectetResult)
+  })
 })
